175eb7f2d25c6b025bfdcb27bd4df87bb1ad4ebb
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-11-24 03:22:04 +0200

    add base page layout and includes for lb-14

---
+++ lb-14/tabulate-function.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tabulate function</title>
+</head>
+<body>
+  <?php include '../includes/navigation-bar.php'; ?>
+  <?php include '../includes/developer-info.php'; ?>
+  <!-- Implementation of this page will be available soon -->
+   
+</body>
+</html>
